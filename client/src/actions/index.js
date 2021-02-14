14f8208a7ecb3d1ae45fc853340eb143ad1fde14
joshi22neha <joshi22neha@gmail.com>
2021-02-14 18:43:54 +0530

    second commit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -14,11 +14,33 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, userId });
     dispatch({
       type: "CREATE_STREAM",
       payload: response.data,
     });
+    //programmatic navigation to get the user back to the root route
   };
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: "FETCH_STREAM", payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: "EDIT_STREAM", payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: "DELETE_STREAM", payload: id });
 };
